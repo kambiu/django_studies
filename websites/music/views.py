3e4caba2578b10427592fd7fc6f6a35e34aaeff1
= <=>
2016-10-15 12:37:56 +0800

    Added Http404 & template render shortcut

--- websites/music/views.py
+++ websites/music/views.py
@@ -1,18 +1,28 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
+# from django.template import loader
+from django.shortcuts import render
 from .models import Album
+from django.http import Http404
 # Create your views here.
 
 def index(request):
 
     all_album = Album.objects.all()
-    template = loader.get_template('music/index.html')
+    # choose default directory /templates/ <- not need to specify
+    # template = loader.get_template('music/index.html')
     context = {
         'all_album': all_album,
     }
+    return render(request, 'music/index.html', context)
+
+    # pass a dict to the html page
     return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album ID:" + str(album_id) + " </h2>")
+    # return HttpResponse("<h2>Details for Album ID:" + str(album_id) + " </h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album})
